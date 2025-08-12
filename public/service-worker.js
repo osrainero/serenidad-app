7be7d8236221abc0fa08dcf76ddcdc9b68c484ab
osrainero <osvaldo.rainero@gmail.com>
2025-08-12 16:29:41 -0300

    Refactor: Service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+const CACHE_NAME = 'serenidad-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/bundle.js',
+  '/static/css/main.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
