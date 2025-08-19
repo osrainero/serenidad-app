36f29ec8986c1170959934cf5b36adfce55564e8
osrainero <osvaldo.rainero@gmail.com>
2025-08-19 17:06:07 -0300

    Fix: Worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then((cacheNames) => { 
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
